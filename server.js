98cb657fb89eae7acb3e1e2260a02d346906f6d2
root <root@localhost.localdomain>
2017-10-03 19:22:25 +0200

    four commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'hbs');
 app.use((req, res, next)=>{
     var now = new Date().toString();
     var log = `log -> ${now}: ${req.method} ${req.url}`;
-    console.log("log:", log);
+    console.log(log);
 
     fs.appendFile('server.log', log + '\n' , (err) => {
         if (err) throw err;
