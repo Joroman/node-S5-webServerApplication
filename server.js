3f6d452120c1c8e59097a129c4b1fa7461414699
root <root@localhost.localdomain>
2017-10-03 15:20:28 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+// I create a middleware that keep track how the server is working , show on the console and save on file
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `log -> ${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n' , (err) => {
+        if (err) throw err;
+        next();
+    });
+});
+
+//middleware maintenance
+/**app.use((req, res, next) =>{
+    res.render('maintenance.hbs',{
+        title:'Maintenance',
+        message:'The site is currently being updated.'
+    });
+});**/
+
+//add Midelware
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+//Here I create a route that handler for http request
+app.get('/',(req, res)=>{
+    //response.send("<h1>Hello Express</h1>");
+    /**res.send({
+        name:'Andrew',
+        likes:['Biking','Cities']
+    });**/
+    res.render('home.hbs',{
+        pageTitle:"Home Page",
+        welcomeMessage:'Welcome to the Home page'
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle:"Abotu Page"
+    });
+});
+
+//Unable to fulfill this request
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage:"Unable to handle request"
+    });
+});
+
+//make the server listen and defien the port 
+app.listen(3000, ()=>{
+    console.log("The server is up on port 3000");
+});
